a780b0dab7b059b1e1c9ecd0139bc8e933b60913
Murhaf Shaker <murhaf.shaker@medicus.ai>
2023-07-31 13:33:51 +0300

    change job interval

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new PopulateNews())->hourly();
+        $schedule->job(new PopulateNews())->everyFiveMinutes();
     }
 
     /**
